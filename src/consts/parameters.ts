6b4e159320fc8c89f3e175c60a4531d948ebedbb
aungkokothet <aungkokothet@gmail.com>
2024-11-11 04:48:00 +0630

    Update parameters.ts

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -1,5 +1,5 @@
 import { createThirdwebClient, getContract } from "thirdweb";
-import { ethereum } from "thirdweb/chains";
+import { polygon-zkevm-cardona-testnet } from "thirdweb/chains";
 
 /** Change these values to configure the application for your own use. **/
 
@@ -9,12 +9,12 @@ export const client = createThirdwebClient({
 
 export const nftContract = getContract({
   // Your smart contract address (available on the thirdweb dashboard)
-  address: "0xed5af388653567af2f388e6224dc7c4b3241c544",
+  address: "0x440fbb3e61A30973C3471b48eEC02020a1fbC9e7",
   // The chain object of the chain your contract is deployed to.
   // If that chain isn't in the default list of our SDK, use `defineChain` - for example: defineChain(666666)
-  chain: ethereum,
+  chain: polygon-zkevm-cardona-testnet,
   client,
 });
 
 // The block explorer you want to use (Opens when user clicks on history of events. i.e. transfers)
-export const blockExplorer = "https://etherscan.io";
+export const blockExplorer = "https://cardona-zkevm.polygonscan.com/";
