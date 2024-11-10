084e7805fb396c96a95955113533ff2e67574abb
aungkokothet <aungkokothet@gmail.com>
2024-11-11 05:10:10 +0630

    Update parameters.ts

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -1,34 +1,21 @@
-import { createThirdwebClient, getContract, defineChain } from "thirdweb";
-import type { Chain } from "thirdweb/types";
+import { createThirdwebClient, getContract } from "thirdweb";
+import { defineChain } from "thirdweb";
+const myChain = defineChain(2442);
 
-/** Define the Polygon zkEVM Cardona Testnet as a custom chain **/
-export const cardonaTestnet: Chain = defineChain({
-  id: 2442,
-  name: "Polygon zkEVM Cardona Testnet",
-  network: "polygon-zkevm-cardona-testnet",
-  nativeCurrency: {
-    name: "Ether",
-    symbol: "ETH",
-    decimals: 18,
-  },
-  rpcUrls: ["https://rpc.cardona.zkevm-rpc.com", "https://2442.rpc.thirdweb.com/${THIRDWEB_API_KEY}"],
-  blockExplorers: {
-    default: { name: "polygonscan", url: "https://cardona-zkevm.polygonscan.com" },
-  },
-  testnet: true,
-});
+/** Change these values to configure the application for your own use. **/
 
-/** Initialize the thirdweb client **/
 export const client = createThirdwebClient({
   clientId: import.meta.env.VITE_TEMPLATE_CLIENT_ID,
 });
 
-/** Set up your NFT contract using the defined custom chain **/
 export const nftContract = getContract({
+  // Your smart contract address (available on the thirdweb dashboard)
   address: "0x440fbb3e61A30973C3471b48eEC02020a1fbC9e7",
-  chain: cardonaTestnet,
+  // The chain object of the chain your contract is deployed to.
+  // If that chain isn't in the default list of our SDK, use `defineChain` - for example: defineChain(666666)
+  chain: myChain,
   client,
 });
 
-/** Optional block explorer link **/
+// The block explorer you want to use (Opens when user clicks on history of events. i.e. transfers)
 export const blockExplorer = "https://cardona-zkevm.polygonscan.com";
